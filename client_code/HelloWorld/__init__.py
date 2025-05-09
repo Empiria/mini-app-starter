4ab3b4f4dd3cd31badacbcde655b20540a7403d2
Owen Campbell <owen.campbell@empiria.co.uk>
2025-05-09 13:38:34 +0100

    Add radicle, github and anvil clone links

--- client_code/HelloWorld/__init__.py
+++ client_code/HelloWorld/__init__.py
@@ -8,9 +8,9 @@ I'm a mini-app starter kit created by @meatballs, @theref and @uglyfruitcake of 
 
 I'm written (almost) entirely in Python using the [Anvil](https://anvil.works) framework.
 
-My source code is available on radicle or, if you insist, on github too.
+My source code is available on [radicle](git@github.com:Empiria/mini-app-starter.git) or, if you insist, on [github](https://github.com/empiria/mini-app-starter) too.
 
-If you have an Anvil account, you can clone me.
+If you have an Anvil account, you can [clone](https://anvil.works/build#clone:PULS75XJLCWFIACD=DE3LKNF7IJER4OJWUZNRTMO6) me on there.
 """
 
 
