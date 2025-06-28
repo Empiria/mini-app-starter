e905b6a417a117f91e7989132e9e8b53566ae605
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2025-06-28 09:03:58 +0000

    Edited services.routes

--- client_code/services/routes.py
+++ client_code/services/routes.py
@@ -1,20 +1,5 @@
 from routing import router
 
-embed = {
-    "version": "next",
-    "imageUrl": "https://mini-app-starter.anvil.app/_/theme/warpcast.jpeg",
-    "button": {
-        "title": "🚩 Start",
-        "action": {
-            "type": "launch_frame",
-            "url": "https://mini-app-starter.anvil.app",
-            "name": "Mini App Starter",
-            "splashImageUrl": "https://mini-app-starter.anvil.app/_/theme/warpast.jpeg",
-            "splashBackgroundColor": "#f5f0ec"
-        }
-    }
-}
-
 
 class HelloRoute(router.Route):
     path = "/"
