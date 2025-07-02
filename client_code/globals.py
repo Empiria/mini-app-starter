112ecba4980e1ca0b2a08cda9200a4c6b0d302ee
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2025-07-02 07:54:21 +0000

    Edited globals

--- client_code/globals.py
+++ client_code/globals.py
@@ -17,12 +17,12 @@ class _Session:
             return
         self.logger.debug("launching session...")
         self.terminate()
-        # router.launch()
+        router.launch()
         self.launched = True
         self.logger.debug("session launched")
 
     def terminate(self):
-        # router.clear_cache()
+        router.clear_cache()
         self.logger.debug("session terminated")
 
 
