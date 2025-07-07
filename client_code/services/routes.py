1c0bd597bc9cf9fb92625f52192e91d813861e00
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2025-07-07 11:26:20 +0000

    Edited services.routes

--- client_code/services/routes.py
+++ client_code/services/routes.py
@@ -1,6 +1,21 @@
 from routing import router
 import json
 
+embed = {
+    "version": "next",
+    "imageUrl": "https://mini-app-starter.anvil.app/_/theme/warpcast.jpeg",
+    "button": {
+        "title": "🚩 Start",
+        "action": {
+            "type": "launch_frame",
+            "url": "https://mini-app-starter.anvil.app",
+            "name": "Mini App Starter",
+            "splashImageUrl": "https://mini-app-starter.anvil.app/_/theme/warpast.jpeg",
+            "splashBackgroundColor": "#f5f0ec"
+        }
+    }
+}
+
 
 class HelloRoute(router.Route):
     path = "/home"
